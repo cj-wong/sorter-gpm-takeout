c1b130295445bfa7a6f7483971a2196100b78a63
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-10-30 16:29:26 -0700

    Add config reader

---
+++ sorter/__init__.py
@@ -0,0 +1,6 @@
+from . import config
+
+
+__all__ = [
+    'config',
+    ]
---
+++ sorter/config.py
@@ -0,0 +1,50 @@
+import json
+import logging
+import logging.handlers
+from pathlib import Path
+
+
+_LOGGER_NAME = 'sorter-gpm'
+
+LOGGER = logging.getLogger(_LOGGER_NAME)
+LOGGER.setLevel(logging.DEBUG)
+
+_FH = logging.handlers.RotatingFileHandler(
+    f'{_LOGGER_NAME}.log',
+    maxBytes=40960,
+    backupCount=5,
+    )
+_FH.setLevel(logging.DEBUG)
+
+_CH = logging.StreamHandler()
+_CH.setLevel(logging.WARNING)
+
+FORMATTER = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+_FH.setFormatter(FORMATTER)
+_CH.setFormatter(FORMATTER)
+
+LOGGER.addHandler(_FH)
+LOGGER.addHandler(_CH)
+
+
+_CONFIG_LOAD_ERRORS = (
+    FileNotFoundError,
+    KeyError,
+    TypeError,
+    ValueError,
+    json.decoder.JSONDecodeError,
+    )
+
+
+try:
+    with open('config.json', 'r') as f:
+        CONFIG = json.load(f)
+    FMT = CONFIG['format']
+    _TAKEOUT = Path(CONFIG['takeout_dir']) 
+    TRACKS = _TAKEOUT / 'Google Play Music' / 'Tracks'
+except _CONFIG_LOAD_ERRORS as e:
+    LOGGER.error('config.json doesn\'t exist or is malformed.')
+    LOGGER.error(f'More information: {e}')
+    raise e
